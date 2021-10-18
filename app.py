872b1d2873bd5e6c5da03124baa06056831f122e
adityarj-pazuzu <aditya.jambhalikar001@gmail.com>
2021-10-18 23:53:53 +0530

    deleted database added file to run app

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_jwt import JWT
 from flask_restful import Api
 
-from db import db
 from resources.item import *
 from resources.store import *
 from resources.user import UserRegister
@@ -14,10 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'ggwp'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
@@ -27,5 +22,4 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == "__main__":
-    db.init_app(app)
     app.run(debug=True)
